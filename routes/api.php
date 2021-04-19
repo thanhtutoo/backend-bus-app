345ce0166c5cbb7e2a0341b2872b655626d7ea44
thanhtutoo <thanhtutoo.dev@gmail.com>
2021-04-19 08:45:32 +0800

    update route

--- routes/api.php
+++ routes/api.php
@@ -18,25 +18,19 @@ use App\Http\Controllers\API\BusTimingController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
   
 Route::post('login', [AuthController::class, 'login']);
 Route::post('refresh', [AuthController::class, 'refresh']);
-// Route::get('/bus-stops', [BusController::class, 'getNearByBusStop']);
-Route::get('/bus-stops/{bus_stop_id}', [BusStopController::class, 'bus_list']);
+
 Route::group([
     'middleware' => ['api','jwt.verify']
  ], function ($router) {
+    Route::get('users', [UserController::class, 'index']);
     Route::get('/bus-stops', [BusStopController::class, 'bus_stops']);
-
+    Route::get('/bus-stops/{bus_stop_id}', [BusStopController::class, 'bus_list']);
     Route::post('logout', [AuthController::class, 'logout']);
-
     Route::post('me', [AuthController::class, 'me']);
     Route::post('users', [UserController::class, 'store']);
-    Route::get('users', [UserController::class, 'index']);
     Route::resource('users', UserController::class);
     Route::resource('bus', BusController::class);
     Route::resource('busstop', BusStopController::class);
